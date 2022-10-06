da548e8dd7a04c7b641a75cc918786caabd061e1
kbannie <kabin2205@gmail.com>
2022-10-06 14:07:53 +0900

    포스트 목록/상세 페이지에 대표이미지 출력

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns={
+urlpatterns=[
     path('',views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
 
     # path('',views.index),  #IP주소/blog  #views 연결하기  #.index : 함수명
     # path('<int:pk>/', views.single_post_page)
-}
+]
